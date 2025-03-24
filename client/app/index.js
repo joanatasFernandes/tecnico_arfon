e77109acf318ee62ceef34e1e9368a266f4c4408
joanatas <joanatasfernandesti@outlook.com>
2025-03-24 19:47:56 -0300

    Melhorias no Js

--- client/app/index.js
+++ client/app/index.js
@@ -1,39 +1,28 @@
-var campos = [
-    document.querySelector('data'),
-    document.querySelector('valor'),
-    document.querySelector('quantidade')
-];
-
-var tbody = document.querySelector('table tbody')
-
-console.log(campos);
-
-var tbody = document.querySelector('table tbody');
-
-document.querySelector('.form').addEventListener('submit', function (event) {
-
-
-    event.preventDefault()
-
-    var tr = document.createElement('tr');
-
-    campos.forEach(function (campo) {
-        var td = document.createElement('td');
-        td.textContent = campo.value;
-        tr.appendChild(td);
+var	campos	=	[
+    document.querySelector('#data'),
+    document.querySelector('#valor'),
+    document.querySelector('#quantidade')
+    ];
+    console.log(campos);	//	verificando	o	conteúdo	do	array
+    var	tbody	=	document.querySelector('table	tbody');
+    document.querySelector('.form').addEventListener('submit',	function(event) {
+            event.preventDefault();
+    var	tr	=	document.createElement('tr');
+	campos.forEach(function(campo) {
+    var	td	=	document.createElement('td');
+                            td.textContent	=	campo.value;
+                            tr.appendChild(td);
+            });
+    var	tdVolume	=	document.createElement('td');
+            tdVolume.textContent	=	campos[1].value	*	campos[2].value;
+            tr.appendChild(tdVolume);		
+            tbody.appendChild(tr);
+    //	limpa	o	campo	da	data
+            campos[0].value	=	'';
+    //	limpa	o	campo	da	quantidade
+            campos[1].value	=	1;
+    //	limpa	o	campo	do	valor
+            campos[2].value	=	0;
+    //	foca	no	campo	da	data
+            campos[0].focus();
     });
-
-    var tdVolume = document.createElement('td');
-    tdVolume.textContent = campos[1].value * campos[2].value;
-    tr.appendChild(tdVolume);
-    tbody.appendChild(tr);
-
-		campos[0].value	=	'';
-
-		campos[1].value	=	1;
-
-		campos[2].value	=	0;
-
-		campos[0].focus();
-
-});
